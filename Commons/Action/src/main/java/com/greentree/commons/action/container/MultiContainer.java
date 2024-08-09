86427845dfd4a1058aba248ea0eac838b8b102a9
Arseny Latyshev <arseny.latyshev@gmail.com>
2024-08-09 23:12:36 +0300

    MultiContainer throw exception

--- Commons/Action/src/main/java/com/greentree/commons/action/container/MultiContainer.java
+++ Commons/Action/src/main/java/com/greentree/commons/action/container/MultiContainer.java
@@ -1,99 +1,120 @@
 package com.greentree.commons.action.container;
 
+import com.greentree.commons.action.ListenerCloser;
+import com.greentree.commons.util.iterator.IteratorUtil;
+
 import java.io.Externalizable;
 import java.io.IOException;
 import java.io.ObjectInput;
 import java.io.ObjectOutput;
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
-
-import com.greentree.commons.action.ListenerCloser;
-
+import java.util.List;
 
 public final class MultiContainer<L> implements ListenerContainer<L>, Externalizable {
-	
-	private static final long serialVersionUID = 1L;
-	
-	private Collection<L> listeners = new WeakMultiSet<>() {
-		
-		protected void weakRemove() {
-			throw new ListenerNotCloseException();
-		};
-		
-	};
-	
-	private Collection<L> _add = new ArrayList<>();
-	private Collection<L> _remove = new ArrayList<>();
-	
-	@Override
-	public ListenerCloser add(L listener) {
-		synchronized(_add) {
-			_add.add(listener);
-		}
-		return new MultiContainerListenerCloser<>(_remove, listener);
-	}
-	
-	@Override
-	public void clear() {
-		synchronized(listeners) {
-			updateListeners();
-			listeners.clear();
-		}
-	}
-	
-	public boolean isEmpty() {
-		synchronized(listeners) {
-			updateListeners();
-			return listeners.isEmpty();
-		}
-	}
-	
-	@Override
-	public Iterator<L> iterator() {
-		synchronized(listeners) {
-			updateListeners();
-			return listeners.iterator();
-		}
-	}
-	
-	@Override
-	public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
-	}
-	
-	@Override
-	public int size() {
-		synchronized(listeners) {
-			updateListeners();
-			return listeners.size();
-		}
-	}
-	
-	@Override
-	public void writeExternal(ObjectOutput out) throws IOException {
-	}
-	
-	private void updateListeners() {
-		synchronized(_add) {
-			listeners.addAll(_add);
-			_add.clear();
-		}
-		synchronized(_remove) {
-			listeners.removeAll(_remove);
-			_remove.clear();
-		}
-	}
-	
-	private record MultiContainerListenerCloser<L>(Collection<L> _remove, L listener)
-			implements ListenerCloser {
-		
-		@Override
-		public void close() {
-			synchronized(_remove) {
-				_remove.add(listener);
-			}
-		}
-		
-	}
-	
+
+    private static final long serialVersionUID = 1L;
+
+    private final List<ListenerInfo<L>> listeners = new ArrayList<>();
+
+    private final Collection<ListenerInfo<L>> _add = new ArrayList<>();
+    private final Collection<L> _remove = new ArrayList<>();
+
+    @Override
+    public ListenerCloser add(L listener) {
+        var info = new ListenerInfo<L>(listener, new ListenerNotCloseException());
+        synchronized (_add) {
+            _add.add(info);
+        }
+        return new MultiContainerListenerCloser<>(_remove, listener);
+    }
+
+    @Override
+    public int size() {
+        synchronized (listeners) {
+            updateListeners();
+            return listeners.size();
+        }
+    }
+
+    @Override
+    public void clear() {
+        synchronized (listeners) {
+            updateListeners();
+            listeners.clear();
+        }
+    }
+
+    private void updateListeners() {
+        synchronized (listeners) {
+            synchronized (_add) {
+                listeners.addAll(_add);
+                _add.clear();
+            }
+            synchronized (_remove) {
+                listeners.removeIf(x -> _remove.contains(x.listener.get()));
+                _remove.clear();
+            }
+            for (ListenerInfo<L> listener : listeners) {
+                if (!listener.isValid())
+                    throw listener.exception;
+
+            }
+        }
+    }
+
+    public boolean isEmpty() {
+        synchronized (listeners) {
+            updateListeners();
+            return listeners.isEmpty();
+        }
+    }
+
+    @Override
+    public Iterator<L> iterator() {
+        synchronized (listeners) {
+            updateListeners();
+            return IteratorUtil.map(listeners, x -> x.listener.get()).iterator();
+        }
+    }
+
+    @Override
+    public void writeExternal(ObjectOutput out) throws IOException {
+    }
+
+    @Override
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+    }
+
+    private record ListenerInfo<L>(
+            WeakReference<L> listener,
+            RuntimeException exception
+    ) {
+
+        ListenerInfo(L listener, RuntimeException exception) {
+            this(new WeakReference<>(listener), exception);
+        }
+
+        public boolean isValid() {
+            return listener.get() != null;
+        }
+
+    }
+
+    private record MultiContainerListenerCloser<L>(
+            Collection<L> _remove,
+            L listener
+    ) implements ListenerCloser {
+
+        @Override
+        public void close() {
+            synchronized (_remove) {
+                _remove.add(listener);
+            }
+        }
+
+    }
+
 }
