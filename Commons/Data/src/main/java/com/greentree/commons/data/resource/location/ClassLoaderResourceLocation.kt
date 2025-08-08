ce6d819a9b7220d30bd7044d51934904771e0d5a
Arseny Latyshev <arseny.latyshev@gmail.com>
2025-08-08 12:14:46 +0300

    Update ClassLoaderResourceLocation.kt

--- Commons/Data/src/main/java/com/greentree/commons/data/resource/location/ClassLoaderResourceLocation.kt
+++ Commons/Data/src/main/java/com/greentree/commons/data/resource/location/ClassLoaderResourceLocation.kt
@@ -1,14 +1,19 @@
 package com.greentree.commons.data.resource.location
 
+import com.greentree.commons.data.resource.FileResource
 import com.greentree.commons.data.resource.Resource
 import com.greentree.commons.data.resource.URLFileResource
 import java.net.URL
 
-class ClassLoaderResourceLocation(private val loader: ClassLoader) : ResourceLocation {
+data class ClassLoaderResourceLocation(val loader: ClassLoader) : ResourceLocation {
 
 	constructor(cls: Class<*> = ClassLoaderResourceLocation::class.java) : this(cls.classLoader)
 
 	override fun getResourceOrNull(name: String): Resource? {
+		TODO("Not yet implemented")
+	}
+
+	override fun getFileResourceOrNull(name: String): FileResource? {
 		val url = getURL(name) ?: return null
 		return URLFileResource(url)
 	}
